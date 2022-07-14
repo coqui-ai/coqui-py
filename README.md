8672354e9bd213e0daed104f403c05e5f06e8435
Reuben Morais <reuben.morais@gmail.com>
2022-07-14 23:55:42 +0200

    API, sync by default, accept file names and file objects, object wrappers, exception wrappers

--- README.md
+++ README.md
@@ -2,7 +2,99 @@
 
 🐸CLI lets you use our services programmatically.
 
-# Development
+## Installation
+
+```bash
+$ pip install coqui
+```
+
+## Usage
+
+From CLI:
+
+```
+$ coqui token-login --login YOUR_API_TOKEN_HERE
+Logged in!
+$ coqui tts get-voices
+[ClonedVoice(id='030527c9-1ae6-4e14-a4de-e063816d0fe4', name='once upon a time', samples_count=3, created_at=datetime.datetime(2022, 7, 13, 18, 38, 8, 725000)), ClonedVoice(id='04dd7d71-f474-45d1-8743-a7fcb4f8b3c4', name='once upon a time 2', samples_count=0, created_at=datetime.datetime(2022, 7, 15, 11, 55, 17, 155000))]
+$ coqui tts clone-voice --audio_file ~/Downloads/blob_PSxUPIV.wav --name "once upon a time 3"
+ClonedVoice(id='4b7a1b08-67f9-4bf2-8600-f97cd39bd39c', name='once upon a time 3', samples_count=0, created_at=datetime.datetime(2022, 7, 15, 12, 37, 3, 111000))
+$ coqui tts synthesize --voice 4b7a1b08-67f9-4bf2-8600-f97cd39bd39c --text "hello from the world of synthesis" --save foo.wav
+Saved synthesized sample to foo.wav
+$ play foo.wav
+
+foo.wav:
+
+ File Size: 175k      Bit Rate: 706k
+  Encoding: Signed PCM
+  Channels: 1 @ 16-bit
+Samplerate: 44100Hz
+Replaygain: off
+  Duration: 00:00:01.99
+
+In:100%  00:00:01.99 [00:00:00.00] Out:95.3k [      |      ] Hd:0.0 Clip:0
+Done.
+$ coqui tts list-samples --voice  4b7a1b08-67f9-4bf2-8600-f97cd39bd39c
+[Sample(id='33115d0a-e555-4fb0-9a66-98edf56143a9', name='hello from the world of synthe', text='hello from the world of synthesis', created_at=datetime.datetime(2022, 7, 15, 12, 40, 19, 981000), audio_url='https://coqui-dev-creator-app-synthesized-samples.s3.amazonaws.com/samples/sample_wqmGJri.wav?AWSAccessKeyId=AKIAXW7NFYT5F2KY3J4D&Signature=wU3Px%2FgnuK7TMghRRFRRPKfUuXs%3D&Expires=1657892449')]
+```
+
+Programmatically:
+
+```python
+from coqui import Coqui
+API_TOKEN = "your token goes here"
+coqui = Coqui()
+coqui.login(API_TOKEN)
+print("\n".join(f"{v.id - v.name}" for v in coqui.cloned_voices()))
+```
+
+Async APIs:
+
+```python
+$ python -m asyncio
+asyncio REPL 3.10.2 (main, Feb  8 2022, 18:36:32) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Use "await" directly instead of "asyncio.run()".
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import asyncio
+>>> from coqui import Coqui
+>>> await coqui.cloned_voices_async()
+Traceback (most recent call last):
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/concurrent/futures/_base.py", line 446, in result
+    return self.__get_result()
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/concurrent/futures/_base.py", line 391, in __get_result
+    raise self._exception
+  File "<console>", line 1, in <module>
+  File "/Users/reuben/Development/coqui-py/coqui/__init__.py", line 225, in cloned_voices
+    async with self._get_session() as session:
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/contextlib.py", line 199, in __aenter__
+    return await anext(self.gen)
+  File "/Users/reuben/Development/coqui-py/coqui/__init__.py", line 164, in _get_session
+    raise AuthenticationError(
+coqui.AuthenticationError: Tried to create authenticated session without logging in.
+>>> # Oops, forgot to login!
+>>> API_TOKEN = "your token goes here"
+>>> await coqui.login_async(API_TOKEN)
+True
+>>> await coqui.cloned_voices_async()
+[ClonedVoice(id='030527c9-1ae6-4e14-a4de-e063816d0fe4', name='once upon a time', samples_count=3, created_at=datetime.datetime(2022, 7, 13, 18, 38, 8, 725000)), ClonedVoice(id='04dd7d71-f474-45d1-8743-a7fcb4f8b3c4', name='once upon a time 2', samples_count=0, created_at=datetime.datetime(2022, 7, 15, 11, 55, 17, 155000)), ClonedVoice(id='4b7a1b08-67f9-4bf2-8600-f97cd39bd39c', name='once upon a time 3', samples_count=5, created_at=datetime.datetime(2022, 7, 15, 12, 37, 3, 111000))]
+>>> coqui.cloned_voices()
+Traceback (most recent call last):
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/concurrent/futures/_base.py", line 446, in result
+    return self.__get_result()
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/concurrent/futures/_base.py", line 391, in __get_result
+    raise self._exception
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/asyncio/__main__.py", line 34, in callback
+    coro = func()
+  File "<console>", line 1, in <module>
+  File "/Users/reuben/Development/coqui-py/coqui/__init__.py", line 44, in sync_func
+    return asyncio.run(meth(*args, **kwargs))
+  File "/Users/reuben/.pyenv/versions/3.10.2/lib/python3.10/asyncio/runners.py", line 33, in run
+    raise RuntimeError(
+RuntimeError: asyncio.run() cannot be called from a running event loop
+>>> # Don't call sync methods in async contexts!
+```
+
+## Development
 
 ```bash
 $ python -m pip install flit
--- coqui/__main__.py
+++ coqui/__main__.py
@@ -5,16 +5,13 @@ import shutil
 import subprocess
 import sys
 import tempfile
-from contextlib import asynccontextmanager
 from functools import wraps
 
-import aiohttp
 import click
-from gql import gql, Client
-from gql.transport.aiohttp import AIOHTTPTransport
+from . import Coqui
 
 
-def coro(f):
+def coroutine(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         return asyncio.run(f(*args, **kwargs))
@@ -44,33 +41,8 @@ class PersistedConfig:
             return json.load(fin)
 
 
+BASE_URL = "http://localhost:8001"
 AuthInfo = PersistedConfig("~/.coqui/credentials")
-
-
-def is_signed_in():
-    # TODO: verify token
-    auth = AuthInfo.get()
-    return auth and auth["token"]
-
-
-@asynccontextmanager
-async def get_session(authed=True):
-    headers = None
-
-    if authed:
-        if not is_signed_in():
-            raise RuntimeError(
-                "Tried to create authenticated session without logging in."
-            )
-
-        token = AuthInfo.get()["token"]
-        headers = {"Authorization": f"JWT {token}"}
-
-    transport = AIOHTTPTransport(url="http://localhost:8001/graphql/", headers=headers)
-    async with Client(
-        transport=transport,
-    ) as session:
-        yield session
 
 
 @click.group()
@@ -83,7 +55,7 @@ def main():
 @click.option("--username", help="Username to login as")
 @click.option("--password", help="Password", default=None)
 @click.option("--password-stdin", help="Read password from stdin", is_flag=True)
-@coro
+@coroutine
 async def login(username, password, password_stdin):
     if password_stdin:
         password = sys.stdin.read().strip()
@@ -91,44 +63,25 @@ async def login(username, password, password_stdin):
     if not password:
         raise RuntimeError("Sign in requires either --password or --password-stdin")
 
-    async with get_session(authed=False) as session:
-        mutation = gql(
-            """
-            mutation Login($username: String!, $password: String!) {
-                tokenAuth(username: $username, password: $password) {
-                    token
-                    refresh_token
-                    payload
-                }
-            }
-        """
-        )
-        result = await session.execute(
-            mutation,
-            variable_values={
-                "username": username,
-                "password": password,
-            },
-        )
-        AuthInfo.set(result["tokenAuth"])
-        click.echo("Signed in!")
+    coqui = Coqui(base_url=BASE_URL)
+    try:
+        token = await coqui.password_login_async(username, password)
+        AuthInfo.set(token)
+        click.echo("Logged in!")
+    except:
+        click.echo("Error: Invalid credentials!")
 
 
 @main.command()
 @click.option("--token", help="API token to sign in with")
-@coro
+@coroutine
 async def token_login(token):
-    AuthInfo.set({"token": token})
-    async with get_session() as session:
-        query = gql(
-            """{
-            profile {
-                email
-            }
-        }"""
-        )
-        result = await session.execute(query)
-        click.echo("Signed in!")
+    coqui = Coqui(base_url=BASE_URL)
+    if await coqui.login_async(token):
+        AuthInfo.set(token)
+        click.echo("Logged in!")
+    else:
+        click.echo("Error: Invalid token!")
 
 
 @main.group()
@@ -137,99 +90,34 @@ def tts():
 
 
 @tts.command()
-@coro
+@coroutine
 async def get_voices():
-    async with get_session() as session:
-        query = gql(
-            """{
-            voices {
-                id
-                name
-                samples_count
-            }
-        }"""
-        )
-
-        result = await session.execute(query)
-        click.echo(result)
-
-
-async def download_file(url, f, chunk_size=5 * 2**20):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data_to_read = True
-            while data_to_read:
-                data = bytearray()
-                red = 0
-                while red < chunk_size:
-                    chunk = await response.content.read(chunk_size - red)
-                    if not chunk:
-                        data_to_read = False
-                        break
-                    data.extend(chunk)
-                    red += len(chunk)
-
-                f.write(data)
+    coqui = Coqui(base_url=BASE_URL)
+    await coqui.login_async(AuthInfo.get())
+    voices = await coqui.cloned_voices_async()
+    click.echo(voices)
 
 
 @tts.command()
 @click.option("--audio_file", help="Path of reference audio file to clone voice from")
 @click.option("--name", help="Name of cloned voice")
-@coro
+@coroutine
 async def clone_voice(audio_file, name):
-    async with get_session() as session:
-        mutation = gql(
-            """
-            mutation CreateVoice($name: String!, $voice: Upload!) {
-                createVoice(name: $name, voice: $voice) {
-                    errors {
-                        field
-                        errors
-                    }
-                    voice {
-                        id
-                        name
-                        created_at
-                    }
-                }
-            }
-        """
-        )
-        with open(audio_file, "rb") as fin:
-            result = await session.execute(
-                mutation,
-                variable_values={
-                    "voice": fin,
-                    "name": name,
-                },
-                upload_files=True,
-            )
-        click.echo(result)
+    coqui = Coqui(base_url=BASE_URL)
+    await coqui.login_async(AuthInfo.get())
+    with open(audio_file, "rb") as fin:
+        result = await coqui.clone_voice_async(fin, name)
+    click.echo(result)
 
 
 @tts.command()
 @click.option("--voice", help="ID of voice to list existing samples for")
-@coro
+@coroutine
 async def list_samples(voice):
-    async with get_session() as session:
-        query = gql(
-            """
-            query Samples($voice_id: String!) {
-                samples(voice_id: $voice_id) {
-                    id
-                    name
-                    audio_url
-                }
-            }
-        """
-        )
-        result = await session.execute(
-            query,
-            variable_values={
-                "voice_id": voice,
-            },
-        )
-        click.echo(result)
+    coqui = Coqui(base_url=BASE_URL)
+    await coqui.login_async(AuthInfo.get())
+    result = await coqui.list_samples_async(voice_id=voice)
+    click.echo(result)
 
 
 @tts.command()
@@ -242,48 +130,21 @@ async def list_samples(voice):
     help="If specified, save the synthesized sample instead of playing it",
     default=None,
 )
-@coro
+@coroutine
 async def synthesize(voice, text, speed, name, save):
-    async with get_session() as session:
-        mutation = gql(
-            """
-            mutation createSample($name: String!, $voice_id: String!, $text: String!, $speed: String!) {
-                createSample(name: $name, voice_id: $voice_id, text: $text, speed: $speed) {
-                    errors {
-                        field
-                        errors
-                    }
-                    sample {
-                        id
-                        name
-                        created_at
-                        voice_id
-                        audio_url
-                    }
-                }
-            }
-        """
-        )
-        result = await session.execute(
-            mutation,
-            variable_values={
-                "voice_id": str(voice),
-                "text": text,
-                "speed": str(speed),
-                "name": name or text[:30],
-            },
-        )
-        # result = {'createSample': {'errors': None, 'sample': {'id': '62151ee3-858f-4398-935d-e48481263927', 'name': 'test from the command line', 'created_at': '2022-06-14T20:15:33.016Z', 'voice_id': 'c97d34da-a677-4219-b4b2-9ec198c948e0', 'audio_url': 'https://coqui-dev-creator-app-synthesized-samples.s3.amazonaws.com/samples/sample_GAh7vFe.wav?AWSAccessKeyId=AKIAXW7NFYT5F2KY3J4D&Signature=CCz46wpRIHrkBT9TCx4vZMVkAQE%3D&Expires=1655241335'}}}
-        audio_url = result["createSample"]["sample"]["audio_url"]
-        with tempfile.NamedTemporaryFile("wb") as fout:
-            await download_file(audio_url, fout)
-            if save:
-                shutil.copy(fout.name, save)
-                click.echo(f"Saved synthesized sample to {save}")
-            else:
-                subprocess.run(
-                    ["play", fout.name],
-                    check=True,
-                    stdin=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                )
+    coqui = Coqui(base_url=BASE_URL)
+    await coqui.login_async(AuthInfo.get())
+    sample = await coqui.synthesize_async(voice, text, speed, name or text[:30])
+    # sample = {'id': '62151ee3-858f-4398-935d-e48481263927', 'name': 'test from the command line', 'created_at': '2022-06-14T20:15:33.016Z', 'voice_id': 'c97d34da-a677-4219-b4b2-9ec198c948e0', 'audio_url': 'https://coqui-dev-creator-app-synthesized-samples.s3.amazonaws.com/samples/sample_GAh7vFe.wav?AWSAccessKeyId=AKIAXW7NFYT5F2KY3J4D&Signature=CCz46wpRIHrkBT9TCx4vZMVkAQE%3D&Expires=1655241335'}
+    with tempfile.NamedTemporaryFile("wb") as fout:
+        await sample.download_async(fout)
+        if save:
+            shutil.copy(fout.name, save)
+            click.echo(f"Saved synthesized sample to {save}")
+        else:
+            subprocess.run(
+                ["play", fout.name],
+                check=True,
+                stdin=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
